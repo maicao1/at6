ed399e9a59f19ec8a8ca95c3c785dc87e6d35857
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2025-07-02 16:20:27 -0300

    Add files via upload

---
+++ at.js
@@ -0,0 +1,14 @@
+let mangas = ['death note','jojo','tokyo ghoul'];
+mangas.shift();
+console.log(mangas);
+console.log(mangas.length);
+
+let jogos = ['the wicher 3','stardew valley', 'terraria']
+jogos.shift();
+console.log(jogos);
+console.log(jogos.length);
+
+let linguagem = ['html','css', 'js']
+linguagem.shift();
+console.log(linguagem);
+console.log(linguagem.length);
